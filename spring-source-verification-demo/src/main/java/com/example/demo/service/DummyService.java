06e1894c1909a1e96ff4324637b616e502375d1f
minion-K <rnjsalsgus06@naver.com>
2025-10-31 11:32:02 +0900

    1031 Test

---
+++ spring-source-verification-demo/src/main/java/com/example/demo/service/DummyService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyService {
+
+    public void checkNull() {
+        String str = "";
+        System.out.println(str.length());
+    }
+}
